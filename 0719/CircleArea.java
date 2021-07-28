d6cd03cdeaf6a6a7423da3690fb30f4d7f9f2445
오윤택 <xoem00@gmail.com>
2021-07-28 10:25:23 +0000

    Upload New File

---
+++ 0719/CircleArea.java
@@ -0,0 +1,9 @@
+package com.java.first;
+
+public class CircleArea {
+	public static void main(String[] args) {
+		int r = 5;
+		
+		System.out.println("반지름이 5cm인 원의 넓이는 "+ (r*r*3.14)+"Cm^2입니다.");
+	}
+}
