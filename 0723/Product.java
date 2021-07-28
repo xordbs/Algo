b067893664c810675d1ee8b58aea40d10f423b3d
오윤택 <xoem00@gmail.com>
2021-07-28 10:29:59 +0000

    Upload New File

---
+++ 0723/Product.java
@@ -0,0 +1,60 @@
+package com.ssafy.abc;
+
+public class Product {
+	private int num;
+	private String name;
+	private int price;
+	private int qty;
+	
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getNum() {
+		return num;
+	}
+
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public int getQty() {
+		return qty;
+	}
+
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+
+	@Override
+	public String toString() {
+		return "SN = " + num + ", Name = " + name + ", Price = " + price + ", Qty = " + qty+", ";
+	}
+	
+	
+}
